83658b9f2367380fb3378f72c9001325e1201ffa
Mauro Romito <maurovelin@yahoo.com>
2019-10-18 23:09:48 +0200

    setup of gesture recognizers, next step is setting up dependency injection

--- Stella McCartney/Controllers/MenuViewController.swift
+++ Stella McCartney/Controllers/MenuViewController.swift
@@ -15,8 +15,38 @@ protocol MainViewProtocol where Self: UIViewController {
 
 class MenuViewController: UIViewController, Storyboarded {
     
+    @IBOutlet weak var beautyView: UIView!
+    @IBOutlet weak var lingerieView: UIView!
+    @IBOutlet weak var readyToWearView: UIView!
+    @IBOutlet weak var accessoriesView: UIView!
+    
     override func viewDidLoad() {
         navigationController?.setNavigationBarHidden(true, animated: false)
+        setupGestureRecognizers()
+    }
+    
+    private func setupGestureRecognizers() {
+        let beautyViewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapOnBeautyView(_:)))
+         let readyToWearViewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapOnReadyToWearView(_:)))
+         let accessoriesViewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapOnAccessoriesView(_:)))
+         let lingerieViewGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapOnLingerieView(_:)))
+        
+        beautyView.addGestureRecognizer(beautyViewGestureRecognizer)
+        lingerieView.addGestureRecognizer(lingerieViewGestureRecognizer)
+        readyToWearView.addGestureRecognizer(readyToWearViewGestureRecognizer)
+        accessoriesView.addGestureRecognizer(accessoriesViewGestureRecognizer)
+    }
+    
+    @objc private func didTapOnBeautyView(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @objc private func didTapOnReadyToWearView(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @objc private func didTapOnLingerieView(_ sender: UITapGestureRecognizer) {
+    }
+    
+    @objc private func didTapOnAccessoriesView(_ sender: UITapGestureRecognizer) {
     }
     
 }
