e5a551c5713f00e8629772a8e2e04f88a385d988
Mauro Romito <maurovelin@yahoo.com>
2019-10-21 03:37:19 +0200

    i think is perfect now

--- Stella McCartney/UIClasses/ProductImagesFlowLayout.swift
+++ Stella McCartney/UIClasses/ProductImagesFlowLayout.swift
@@ -10,27 +10,28 @@ import Foundation
 import UIKit
 
 class ProductImagesFlowLayout: UICollectionViewFlowLayout {
-
+    
     var activeDistance: CGFloat = 0
     
     //where 1 example is +100% size at center
     let zoomFactor: CGFloat = 1
-    //where 0.5 for example is 30% of alpha outisde the center
-    let alphaFactor: CGFloat = 0.3
-
+    
+    //complementary value of alpha 0.7 means that you have 30% of alpha outside center
+    let transparencyFactor: CGFloat = 0.7
+    
     override init() {
         super.init()
         scrollDirection = .horizontal
         minimumLineSpacing = 50
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func prepare() {
         guard let collectionView = collectionView else { fatalError() }
-        activeDistance = itemSize.width * zoomFactor
+        activeDistance = itemSize.width * ( 1 + zoomFactor) + minimumLineSpacing/2
         
         var verticalInsets = CGFloat.zero
         if #available(iOS 11.0, *) {
@@ -43,73 +44,64 @@ class ProductImagesFlowLayout: UICollectionViewFlowLayout {
         if #available(iOS 11.0, *) {
             horizontalInsets = (collectionView.frame.width - collectionView.adjustedContentInset.right - collectionView.adjustedContentInset.left - itemSize.width) / 2
         } else {
-             horizontalInsets = (collectionView.frame.width - collectionView.contentInset.right - collectionView.contentInset.left - itemSize.width) / 2
+            horizontalInsets = (collectionView.frame.width - collectionView.contentInset.right - collectionView.contentInset.left - itemSize.width) / 2
         }
         sectionInset = UIEdgeInsets(top: verticalInsets, left: horizontalInsets, bottom: verticalInsets, right: horizontalInsets)
-
+        
         super.prepare()
     }
-
+    
     override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
         guard let collectionView = collectionView else { return nil }
         let rectAttributes = super.layoutAttributesForElements(in: rect)!.map { $0.copy() as! UICollectionViewLayoutAttributes }
         let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.frame.size)
-
-        // Make the cells be zoomed when they reach the center of the screen
-        for attributes in rectAttributes where attributes.frame.intersects(visibleRect) {
-            let distance = visibleRect.midX - attributes.center.x
-            let normalizedDistance = distance / activeDistance
-
-            if distance.magnitude < activeDistance {
-                let zoom = 1 + zoomFactor * (1 - normalizedDistance.magnitude)
-                attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 1)
-                attributes.zIndex = Int(zoom.rounded())
-            }
-        }
         
         for attributes in rectAttributes {
             let distance = visibleRect.midX - attributes.center.x
             let normalizedDistance = distance / activeDistance
-
-            if distance.magnitude < activeDistance {
-                let alpha =  1 - alphaFactor * normalizedDistance.magnitude
+            
+            if distance.magnitude < activeDistance  {
+                let zoom = 1 + zoomFactor * (1 - normalizedDistance.magnitude)
+                attributes.transform3D = CATransform3DMakeScale(zoom, zoom, 1)
+                attributes.zIndex = Int(zoom.rounded())
+                let alpha =  1 - transparencyFactor * normalizedDistance.magnitude
                 attributes.alpha = alpha
             } else {
-                attributes.alpha = alphaFactor
+                attributes.alpha = 1 - transparencyFactor
             }
         }
         return rectAttributes
     }
-
+    
     override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
         guard let collectionView = collectionView else { return .zero }
-
+        
         // Add some snapping behaviour so that the zoomed cell is always centered
         let targetRect = CGRect(x: proposedContentOffset.x, y: 0, width: collectionView.frame.width, height: collectionView.frame.height)
         guard let rectAttributes = super.layoutAttributesForElements(in: targetRect) else { return .zero }
-
+        
         var offsetAdjustment = CGFloat.greatestFiniteMagnitude
         let horizontalCenter = proposedContentOffset.x + collectionView.frame.width / 2
-
+        
         for layoutAttributes in rectAttributes {
             let itemHorizontalCenter = layoutAttributes.center.x
             if (itemHorizontalCenter - horizontalCenter).magnitude < offsetAdjustment.magnitude {
                 offsetAdjustment = itemHorizontalCenter - horizontalCenter
             }
         }
-
+        
         return CGPoint(x: proposedContentOffset.x + offsetAdjustment, y: proposedContentOffset.y)
     }
-
+    
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
         // Invalidate layout so that every cell get a chance to be zoomed when it reaches the center of the screen
         return true
     }
-
+    
     override func invalidationContext(forBoundsChange newBounds: CGRect) -> UICollectionViewLayoutInvalidationContext {
         let context = super.invalidationContext(forBoundsChange: newBounds) as! UICollectionViewFlowLayoutInvalidationContext
         context.invalidateFlowLayoutDelegateMetrics = newBounds.size != collectionView?.bounds.size
         return context
     }
-
+    
 }
