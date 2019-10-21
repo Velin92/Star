866d7c5332e7b6582bde41aced46e511ba5ae2e4
Mauro Romito <maurovelin@yahoo.com>
2019-10-21 02:08:02 +0200

    making the view for the images with a custom flow layout , with a zoom and alpha factor that changes as you scroll... yeah I really like collectionViews...

--- Stella McCartney/Controllers/ProductDetailViewController.swift
+++ Stella McCartney/Controllers/ProductDetailViewController.swift
@@ -15,12 +15,47 @@ protocol ProductDetailViewProtocol where Self: UIViewController {
 
 class ProductDetailViewController: UIViewController, Storyboarded {
     
+    @IBOutlet weak var productImagesCollectionView: UICollectionView!
+    
     var viewModel: ProductDetailViewModelProtocol!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       setupProductImagesCollectionView()
         viewModel.loadAdditionalDetails()
     }
+    
+    private func setupProductImagesCollectionView() {
+        productImagesCollectionView.collectionViewLayout = ProductImagesFlowLayout()
+        if #available(iOS 11.0, *) {
+            productImagesCollectionView.contentInsetAdjustmentBehavior = .always
+        }
+        productImagesCollectionView.delegate = self
+        productImagesCollectionView.dataSource = self
+    }
+}
+
+extension ProductDetailViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductImageCell", for: indexPath) as? ProductImageCollectionViewCell else {
+            fatalError("Cell is not setup correctly in storyboard")
+        }
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 70, height: 70)
+    }
+    
 }
 
 extension ProductDetailViewController: ProductDetailViewProtocol {
---
+++ Stella McCartney/UIClasses/ProductImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductImageCollectionViewCell.swift
+//  Stella McCartney
+//
+//  Created by Mauro Romito on 21/10/2019.
+//  Copyright © 2019 Mauro Romito. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProductImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImageView: UIImageView!
+}
